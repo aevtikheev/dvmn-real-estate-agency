94c454342ea0fa302a0afa6f38cb1ba5f6e04a41
aevtikheev <eft000@gmail.com>
2020-12-07 20:13:07 +0300

    Add likes to flats

--- property/models.py
+++ property/models.py
@@ -20,15 +20,19 @@ class Flat(models.Model):
     )
     address = models.TextField("Адрес квартиры", help_text='ул. Подольских курсантов д.5 кв.4')
     floor = models.CharField("Этаж", max_length=3, help_text='Первый этаж, последний этаж, пятый этаж')
-
     rooms_number = models.IntegerField("Количество комнат в квартире", db_index=True)
     living_area = models.IntegerField("количество жилых кв.метров", null=True, blank=True, db_index=True)
-
     has_balcony = models.NullBooleanField("Наличие балкона", db_index=True)
     active = models.BooleanField("Активно-ли объявление", db_index=True)
     construction_year = models.IntegerField("Год постройки здания", null=True, blank=True, db_index=True)
-
     new_building = models.NullBooleanField('Новостройка')
+    liked_by = models.ManyToManyField(
+        User,
+        related_name="liked_flats",
+        null=True,
+        blank=True,
+        verbose_name="Кто лайкнул"
+    )
 
     def __str__(self):
         return f"{self.town}, {self.address} ({self.price}р.)"
@@ -49,11 +53,7 @@ class Complaint(models.Model):
         related_name='complaints',
         verbose_name='Квартира, на которую пожаловались'
     )
-    text = models.TextField(
-        "Текст жалобы",
-        max_length=500,
-        null=True
-    )
+    text = models.TextField("Текст жалобы", max_length=500, null=True)
 
     def __str__(self):
         return f"{self.flat}, {self.user}"
